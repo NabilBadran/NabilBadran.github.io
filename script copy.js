d40ab97862da201d529226676af6cb10009f9a24
Nabil <116255166+NabilBadran@users.noreply.github.com>
2023-09-20 18:31:57 +0100

    Add files via upload

--- script copy.js
+++ script copy.js
@@ -6,6 +6,31 @@
 
 // gallery js code
 
+
+const switchTheme = (event) => {
+  event.preventDefault(); // Prevent the default link behavior
+
+  const rootElem = document.documentElement;
+  let dataTheme = rootElem.getAttribute('data-theme');
+
+  newTheme = (dataTheme === 'light') ? 'dark' : 'light';
+
+  rootElem.setAttribute('data-theme', newTheme);
+
+  // set the new local stoarage item
+  localStorage.setItem('theme',newTheme)
+};
+
+document.querySelector('#theme-switcher').addEventListener('click', switchTheme);
+
+
+
+const featureComingSoonBtn = document.getElementById('featureComingSoonBtn');
+
+featureComingSoonBtn.addEventListener('click', async () => {
+  const result = confirm("This feature will be added soon. Thank you for your patience");
+})
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -34,28 +59,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-
-
-const switchTheme = (event) => {
-  event.preventDefault(); // Prevent the default link behavior
-
-  const rootElem = document.documentElement;
-  let dataTheme = rootElem.getAttribute('data-theme');
-
-  newTheme = (dataTheme === 'light') ? 'dark' : 'light';
-
-  rootElem.setAttribute('data-theme', newTheme);
-
-  // set the new local stoarage item
-  localStorage.setItem('theme',newTheme)
-};
-
-document.querySelector('#theme-switcher').addEventListener('click', switchTheme);
-
-
-
-const featureComingSoonBtn = document.getElementById('featureComingSoonBtn');
-
-featureComingSoonBtn.addEventListener('click', async () => {
-  const result = confirm("This feature will be added soon. Thank you for your patience");
-})
